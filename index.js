499e9d853be9c0fde6c2bfa4dd7697a4b4a6a1df
Emmanuel Chijioke <manuelckay@gmail.com>
2020-03-26 17:51:10 +0100

    JS task

---
+++ index.js
@@ -0,0 +1,10 @@
+var firstName = "Emmanuel"
+var lastName = "    Chijioke"
+console.log (firstName +     lastName)
+var courses = ["HTML", "CSS", "JavaScript", "Python",];
+console.log (courses)
+for (let i = 1; i <= 200; i++){
+    if (i % 2 !== 1)
+
+        console.log(i);
+    }
